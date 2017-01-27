73a7a1622b3bf354c8c1b9285cf67ac7339330dd
Gene <ecg8@yahoo.com>
2017-01-27 03:58:26 +0000

    first version

--- server.js
+++ server.js
@@ -1,9 +1,39 @@
 var express = require("express");
 var app = express();
-app.get("/", function (req, res) {
-  res.send("Hello World!");
+var port = process.env.PORT || 8080;
+app.get("/:query", function (req, res) {
+  var query = req.params.query;
+  res.json(service(query));
 });
 //always use port 8080 on c9.io
-app.listen(8080, function () {
-  console.log("Example app listening on port 8080!");
+app.listen(port, function () {
+  console.log("This app is listening on port " + port + ".");
 });
+function service(query) {
+  var response = {"UNIXtime": "null", "REGULARtime": "null"};
+  var thedate;
+  var montharray = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  if (query == "now" || query == "Now" || query == "NOW") {
+    var now = new Date();
+    response.UNIXtime = now.getTime();
+    response.REGULARtime = montharray[now.getMonth()] + " " + now.getDate() + ", " + now.getFullYear();
+    console.log(response);
+    return response;
+  }
+  if (Number.isInteger(parseInt(query))) {
+    thedate = new Date(parseInt(query));
+    response.UNIXtime = query;
+    response.REGULARtime = montharray[thedate.getMonth()] + " " + thedate.getDate() + ", " + thedate.getFullYear();
+    console.log(response);
+    return response;
+  }
+  thedate = new Date(query);
+  if (Number.isInteger(thedate.getTime())) {
+    response.UNIXtime = thedate.getTime();
+    response.REGULARtime = montharray[thedate.getMonth()] + " " + thedate.getDate() + ", " + thedate.getFullYear();
+    console.log(response);
+    return response;
+  }
+  console.log(response);
+  return response;
+}
